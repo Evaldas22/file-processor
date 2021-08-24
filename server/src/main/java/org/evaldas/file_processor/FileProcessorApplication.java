ee7a3a782985e3480adf69fec38a25b2b809a548
Evaldas Suraucius <surauciusevaldas@gmail.com>
2021-08-24 17:53:52 +0300

    Initialize empty spring boot app

---
+++ server/src/main/java/org/evaldas/file_processor/FileProcessorApplication.java
@@ -0,0 +1,13 @@
+package org.evaldas.file_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileProcessorApplication.class, args);
+	}
+
+}
