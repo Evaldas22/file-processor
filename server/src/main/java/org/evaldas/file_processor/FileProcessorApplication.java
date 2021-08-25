494e962f95539e1210e0e4feb3fd6b5c7f7cb142
Evaldas Suraucius <surauciusevaldas@gmail.com>
2021-08-25 18:42:55 +0300

    Add and enable swagger-ui

--- server/src/main/java/org/evaldas/file_processor/FileProcessorApplication.java
+++ server/src/main/java/org/evaldas/file_processor/FileProcessorApplication.java
@@ -2,8 +2,10 @@ package org.evaldas.file_processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FileProcessorApplication {
 
 	public static void main(String[] args) {
