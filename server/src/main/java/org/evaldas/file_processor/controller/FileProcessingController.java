91d5f8d3aa2d3d4c2d0cc05a1516a5e4c4e52c24
Evaldas Suraucius <surauciusevaldas@gmail.com>
2021-08-28 18:01:32 +0300

    Construct 4 response map according to requirements

--- server/src/main/java/org/evaldas/file_processor/controller/FileProcessingController.java
+++ server/src/main/java/org/evaldas/file_processor/controller/FileProcessingController.java
@@ -1,6 +1,7 @@
 package org.evaldas.file_processor.controller;
 
 import lombok.AllArgsConstructor;
+import org.evaldas.file_processor.controller.response.FileProcessingResultResponse;
 import org.evaldas.file_processor.service.FileProcessingService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -18,7 +18,7 @@ public class FileProcessingController {
 	private final FileProcessingService fileProcessingService;
 
 	@PostMapping(value = "process-files", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public Map<String, Integer> processFile(@RequestPart("files") List<MultipartFile> files) {
+	public FileProcessingResultResponse processFile(@RequestPart("files") List<MultipartFile> files) {
 		return fileProcessingService.process(files);
 	}
 }
--- server/src/main/java/org/evaldas/file_processor/service/FileProcessingService.java
+++ server/src/main/java/org/evaldas/file_processor/service/FileProcessingService.java
@@ -1,20 +1,22 @@
 package org.evaldas.file_processor.service;
 
 import lombok.AllArgsConstructor;
+import org.evaldas.file_processor.controller.response.FileProcessingResultResponse;
 import org.evaldas.file_processor.runnable.FileProcessingRunnable;
+import org.evaldas.file_processor.util.MapSortingUtility;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @AllArgsConstructor
 public class FileProcessingService {
 
-	public Map<String, Integer> process(List<MultipartFile> files) {
+	private final MapSortingUtility mapSortingUtility;
+
+	public FileProcessingResultResponse process(List<MultipartFile> files) {
 		Map<String, Integer> wordsFrequency = new ConcurrentHashMap<>();
 		List<Thread> threads = new ArrayList<>();
 
@@ -37,6 +39,35 @@ public class FileProcessingService {
 		long finish = System.currentTimeMillis();
 		System.out.println("File processing took " + (finish - start) + " ms");
 
-		return wordsFrequency;
+		return buildResponseAndSaveResults(mapSortingUtility.sortMapByWordLengthAndAlphabetically(wordsFrequency));
+	}
+
+	private FileProcessingResultResponse buildResponseAndSaveResults(Map<String, Integer> wordsFrequency) {
+		Map<String, Integer> wordFrequenciesFromAtoG = new LinkedHashMap<>();
+		Map<String, Integer> wordFrequenciesFromHtoN = new LinkedHashMap<>();
+		Map<String, Integer> wordFrequenciesFromOtoU = new LinkedHashMap<>();
+		Map<String, Integer> wordFrequenciesFromVtoZ = new LinkedHashMap<>();
+
+		wordsFrequency.forEach((word, frequency) -> {
+			char firstLetter = word.charAt(0);
+			if (firstLetter >= 'a' && firstLetter <= 'g') {
+				wordFrequenciesFromAtoG.put(word, frequency);
+			} else if (firstLetter >= 'h' && firstLetter <= 'n') {
+				wordFrequenciesFromHtoN.put(word, frequency);
+			} else if (firstLetter >= 'o' && firstLetter <= 'u') {
+				wordFrequenciesFromOtoU.put(word, frequency);
+			} else if (firstLetter >= 'v' && firstLetter <= 'z') {
+				wordFrequenciesFromVtoZ.put(word, frequency);
+			}
+		});
+
+		// TODO: save to 4 result file
+
+		return new FileProcessingResultResponse(
+				wordFrequenciesFromAtoG,
+				wordFrequenciesFromHtoN,
+				wordFrequenciesFromOtoU,
+				wordFrequenciesFromVtoZ
+		);
 	}
 }
