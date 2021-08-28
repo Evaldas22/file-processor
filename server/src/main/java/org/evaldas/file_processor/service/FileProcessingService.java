80b105f19481ab72efdc0b1a21594c76637209db
Evaldas Suraucius <surauciusevaldas@gmail.com>
2021-08-28 16:59:43 +0300

    Implement a method to process each file by separate threads

--- server/src/main/java/org/evaldas/file_processor/service/FileProcessingService.java
+++ server/src/main/java/org/evaldas/file_processor/service/FileProcessingService.java
@@ -1,16 +1,42 @@
 package org.evaldas.file_processor.service;
 
+import lombok.AllArgsConstructor;
+import org.evaldas.file_processor.runnable.FileProcessingRunnable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@AllArgsConstructor
 public class FileProcessingService {
 
 	public Map<String, Integer> process(List<MultipartFile> files) {
-		return new HashMap<>();
+		Map<String, Integer> wordsFrequency = new ConcurrentHashMap<>();
+		List<Thread> threads = new ArrayList<>();
+
+		long start = System.currentTimeMillis();
+
+		files.forEach(file -> {
+			Thread t = new Thread(new FileProcessingRunnable(file, wordsFrequency));
+			t.start();
+			threads.add(t);
+		});
+
+		for (Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		long finish = System.currentTimeMillis();
+		System.out.println("File processing took " + (finish - start) + " ms");
+
+		return wordsFrequency;
 	}
 }
