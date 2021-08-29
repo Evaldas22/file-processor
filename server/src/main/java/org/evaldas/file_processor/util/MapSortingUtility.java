c5b18c1645b86ef8d26624e1b8c2e5c09e7083cd
Evaldas Suraucius <surauciusevaldas@gmail.com>
2021-08-29 10:51:14 +0300

    Change map sorting to only alphabetical

--- server/src/main/java/org/evaldas/file_processor/util/MapSortingUtility.java
+++ server/src/main/java/org/evaldas/file_processor/util/MapSortingUtility.java
@@ -11,11 +11,7 @@ public class MapSortingUtility {
 	public Map<String, Integer> sortMapByWordLengthAndAlphabetically(Map<String, Integer> map) {
 		return map.entrySet()
 				.stream()
-				.sorted((e1, e2) -> {
-					if (e1.getKey().length() > e2.getKey().length()) return 1;
-					if (e1.getKey().length() < e2.getKey().length()) return -1;
-					else return e1.getKey().compareTo(e2.getKey());
-				})
+				.sorted(Map.Entry.comparingByKey())
 				.collect(Collectors.toMap(Map.Entry::getKey,
 						Map.Entry::getValue,
 						(e1, e2) -> e1,
