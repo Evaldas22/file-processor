d4283177ddb3811fce2985f828717409d57c39b6
Evaldas Suraucius <surauciusevaldas@gmail.com>
2021-08-29 18:36:02 +0300

    Change from js to jsx type + add logic

---
+++ client/src/components/FileUpload.jsx
@@ -0,0 +1,30 @@
+function FileUpload({ files, selectFiles, processFiles, isFetching }) {
+	const areFilesAdded = () => {
+		return files && files.length > 0;
+	};
+
+	return (
+		<div>
+			<h3>Upload files</h3>
+			<form onSubmit={processFiles}>
+				<input
+					type='file'
+					id='file'
+					multiple
+					name='file'
+					onChange={selectFiles}
+					disabled={isFetching}
+				/>
+				<button
+					type='submit'
+					className='btn btn-info'
+					disabled={!areFilesAdded() || isFetching}
+				>
+					Process Files
+				</button>
+			</form>
+		</div>
+	);
+}
+
+export default FileUpload;
