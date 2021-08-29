e01474afbf24a3d53a84ce6c9f713ba20d2edf55
Evaldas Suraucius <surauciusevaldas@gmail.com>
2021-08-29 12:15:43 +0300

    Create temporary directory if it's not present

--- server/src/main/java/org/evaldas/file_processor/service/StorageService.java
+++ server/src/main/java/org/evaldas/file_processor/service/StorageService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -16,8 +17,12 @@ public class StorageService {
 
 	public String saveListIntoTempFile(List<String> content, String filename) {
 		String path = System.getProperty("java.io.tmpdir") + "/" + tempDirName + "/" + filename;
+		File file = new File(path);
+		if (file.getParentFile().mkdirs()) { // create temporary directory with tempDirName if it doesn't exist
+			System.out.println("Created " + tempDirName + " directorys");
+		}
 
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
 			content.forEach(string -> {
 				try {
 					writer.write(string + "\n");
@@ -28,6 +33,7 @@ public class StorageService {
 		} catch (IOException e) {
 			System.out.printf("Unable to save contents to a file %s: %s%n",
 					path, e.getMessage());
+			return ""; // empty string will indicate that there's no file to download
 		}
 
 		return filename; // Returning filename, because I don't want to expose server location where it's stored
